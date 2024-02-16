2a0cc64ddf5df252d047178edd821dc5370dd42d
Piptr69 <p.paluchh02@gmail.com>
2024-02-16 18:40:32 +0100

    implement clear button

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
 const screen = document.querySelector('#screen');
 const numericButtons = document.querySelectorAll('.nbtn');
 numericButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
+    button.addEventListener('click', () => {
         if (screen.textContent == '0') {
             screen.textContent = button.textContent;
         }
         else {
             screen.textContent = screen.textContent + button.textContent;
         }
-
     });
+});
+
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', () => {
+    screen.textContent = '0';
 });
