7d77e508c3a74e66e5363798b9f00d3c99995e25
Piptr69 <p.paluchh02@gmail.com>
2024-02-16 18:38:30 +0100

    add elseif to event listener

--- js.js
+++ js.js
@@ -2,6 +2,12 @@ const screen = document.querySelector('#screen');
 const numericButtons = document.querySelectorAll('.nbtn');
 numericButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
-        screen.textContent = button.textContent;
+        if (screen.textContent == '0') {
+            screen.textContent = button.textContent;
+        }
+        else {
+            screen.textContent = screen.textContent + button.textContent;
+        }
+
     });
 });
