996fa1216834bb7b8ad72c036e6fd55c26ab8408
Piptr69 <p.paluchh02@gmail.com>
2024-02-16 18:33:51 +0100

    implement numbers 0-9 showing on screen when clicked

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 const screen = document.querySelector('#screen');
 const numericButtons = document.querySelectorAll('.nbtn');
 numericButtons.forEach((button) => {
-
+    button.addEventListener('click', (event) => {
+        screen.textContent = button.textContent;
+    });
 });
