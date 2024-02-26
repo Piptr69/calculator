5f40d0406ee33694b2837764aaec7bb0af7e5b92
Piptr69 <p.paluchh02@gmail.com>
2024-02-26 14:06:10 +0100

    bug fix

--- js.js
+++ js.js
@@ -34,7 +34,7 @@ function subtract(number1, number2) {
 function multiply(number1, number2) {
     return number1 * number2;
 }
-divide
-function subtract(number1, number2) {
+
+function divide(number1, number2) {
     return number1 / number2;
 }
